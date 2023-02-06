f18063e4490d19459b9d4c92edb97b3db89f42a4
Ryan Current <rcurrent37@gmail.com>
2023-02-06 17:21:33 -0500

    Solved 139

---
+++ 139 WordBreak/Solution/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace Solution;
+public class Solution
+{
+    public bool WordBreak(string s, IList<string> wordDict)
+    {
+        for(int i = 0; i < wordDict.Count; i++)
+        {
+            if(s.Length < wordDict[i].Length)
+                continue; 
+            if(s.Equals(wordDict[i]))
+                return true; 
+            if(s.Substring(0, wordDict[i].Length).Equals(wordDict[i]))
+            { 
+                return WordBreak(s.Substring(wordDict[i].Length), wordDict); 
+            }
+        }
+        return false; 
+    }
+
+}
