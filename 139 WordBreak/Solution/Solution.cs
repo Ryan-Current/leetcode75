52bf7c5397abe220d6e11d54bd4b745816f72d59
Ryan Current <rcurrent37@gmail.com>
2023-02-06 21:25:52 -0500

    Solved 139

--- 139 WordBreak/Solution/Solution.cs
+++ 139 WordBreak/Solution/Solution.cs
@@ -3,24 +3,25 @@ public class Solution
 {
     public bool WordBreak(string s, IList<string> wordDict)
     {
-        // check every word in the dictionary against the beginning of the string
-        for(int i = 0; i < wordDict.Count; i++)
+        bool[] dyn = new bool[s.Length + 1]; 
+        // if we reach the last index, then we know it is true
+        dyn[s.Length] = true; 
+        for(int i = s.Length - 1; i >= 0; i--)
         {
-            // skip the word if the string is too short to compare it 
-            if(s.Length < wordDict[i].Length)
-                continue; 
-            // if this word matches the full string, then it can be broken
-            if(s.Equals(wordDict[i]))
-                return true; 
-            // if this word matches the beginning of the string, then remove the beginning and 
-            // continue processing the string
-            if(s.Substring(0, wordDict[i].Length).Equals(wordDict[i]))
-            { 
-                return WordBreak(s.Substring(wordDict[i].Length), wordDict); 
+            foreach(string word in wordDict)
+            {
+                // are there enough characters in s to compare, and are those characters equal
+                if(i + word.Length <= s.Length && s.Substring(i, word.Length).Equals(word))
+                {
+                    // we can match this word. Set this index to the ability to match up to this index
+                    dyn[i] = dyn[i + word.Length]; 
+                }
+                // if 1 word is found to work then we can stop
+                if(dyn[i])
+                    break; 
             }
         }
-        // if the string is not fully matched, then it cannot be split
-        return false; 
+        return dyn[0]; 
     }
 
 }
