5ef14e84e5dab8d82ec5e2240163aba9d60bf74e
Ryan Current <rcurrent37@gmail.com>
2023-02-07 14:35:59 -0500

    Solved 371

---
+++ 371 IntegerSum/Solution/IntSum.cs
@@ -0,0 +1,23 @@
+﻿namespace Solution;
+public class IntSum
+{
+    // binary addition
+    //   0011
+    // + 0001
+    // = 0100
+    // (places for carryover can be found by a & b)
+    // (places for no carryover can be found by a ^ b)
+    public int GetSum(int a, int b) 
+    {
+        // this value will give us where the integer is
+        int carryover = (a & b) << 1; 
+        int sum = a ^ b; 
+        while(carryover != 0)
+        {
+            int temp = sum ^ carryover; 
+            carryover = (sum & carryover) << 1; 
+            sum = temp; 
+        }
+        return sum; 
+    }
+}
