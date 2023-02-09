8a70c2ee67e4a89dd1d3bc5c6d8f779c680a381e
Ryan Current <rcurrent37@gmail.com>
2023-02-09 17:24:09 -0500

    Solved 217

---
+++ 217 ContainsDuplicate/Solution/ContainsDuplicate.cs
@@ -0,0 +1,17 @@
+﻿namespace Solution;
+public class ContainsDuplicate
+{
+    
+    public bool ContainsDuplicate(int[] nums) 
+    {
+        HashSet<int> set = new HashSet<int>(); 
+        foreach(int num in nums)
+        {
+            if(set.Contains(num))
+                return false; 
+            set.Add(num); 
+        }
+        return true; 
+    }
+
+}
