2608e4c459ad1a0b72f45be4c21f25570767b328
Ryan Current <rcurrent37@gmail.com>
2023-02-09 17:15:49 -0500

    Solved 53

--- 53 MaxSubArray/Solution/MaxSubArray.cs
+++ 53 MaxSubArray/Solution/MaxSubArray.cs
@@ -1,13 +1,14 @@
 ﻿namespace Solution;
 public class MaxSubArray
 {
-
+    
     public int MaxSubArray(int[] nums) 
     {
         int champion = int.MinValue; 
         int currentSum = int.MinValue; 
         for(int i = 0; i < nums.Length; i++)
         {
+            // adding to a value that is negative cannot help, and it is better to just leave it at 0
             if(currentSum < 0)
                 currentSum = nums[i]; 
             else 
