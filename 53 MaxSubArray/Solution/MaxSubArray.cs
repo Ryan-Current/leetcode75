00e444b1e6107ba4b8ef8a2ee681f00993b68d35
Ryan Current <rcurrent37@gmail.com>
2023-02-09 17:14:44 -0500

    Solved 53

---
+++ 53 MaxSubArray/Solution/MaxSubArray.cs
@@ -0,0 +1,21 @@
+﻿namespace Solution;
+public class MaxSubArray
+{
+
+    public int MaxSubArray(int[] nums) 
+    {
+        int champion = int.MinValue; 
+        int currentSum = int.MinValue; 
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(currentSum < 0)
+                currentSum = nums[i]; 
+            else 
+                currentSum = nums[i] + currentSum; 
+            if(champion < currentSum)
+                champion = currentSum; 
+        }
+        return champion; 
+    }
+
+}
