d9b39b53ba6d763262ef1ee7a46a9d5ee0c3dd4c
Ryan Current <rcurrent37@gmail.com>
2023-02-09 19:13:37 -0500

    Solved 141

---
+++ 141 LinkedListCycle/Solution/LinkedListCycle.cs
@@ -0,0 +1,52 @@
+﻿namespace Solution;
+public class LinkedListCycle
+{
+    
+    public bool HasCycle(ListNode head) 
+    {
+        HashSet<ListNode> visited = new HashSet<ListNode>(); 
+        ListNode current = head; 
+        while(current != null)
+        {
+            if(visited.Contains(current))
+                return true; 
+            visited.Add(current); 
+            current = current.next; 
+        }
+        return false; 
+    }
+
+
+    // another solution using less memory is to use a slow and a fast node, if there is a loop, 
+    // they will eventually run into each other, and if not, there will be a null next value
+    public bool HasCycle(ListNode head) 
+    {
+        ListNode slow = head; 
+        ListNode fast = head; 
+        while(slow != null && fast != null && fast.next != null)
+        {
+            slow = slow.next; 
+            fast = fast.next.next; 
+            if(slow == fast)
+                return true;  
+        }
+        return false; 
+    }
+
+}
+
+
+
+// Definition for singly-linked list.
+public class ListNode 
+{
+    public int val;
+    public ListNode next;
+
+
+    public ListNode(int x) 
+    {
+        val = x;
+        next = null;
+    }
+}
