bac44774bab628868e3df35c3ea511683474cf78
Ryan Current <rcurrent37@gmail.com>
2024-07-09 18:13:52 -0400

    New: added 1, 23 and 100

---
+++ 1 Two Sum/Solution.py
@@ -0,0 +1,120 @@
+# 1. Two Sum
+# Easy
+# Topics
+# Companies
+# Hint
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,4,5,8,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+ 
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted = nums.copy()
+        sorted.sort()
+
+        left = 0 
+        right = len(sorted) - 1
+
+        while(sorted[left] + sorted[right] != target):
+            if sorted[left] + sorted[right] > target: 
+                right -= 1 
+            else:
+                left += 1
+        
+        first_index = nums.index(sorted[left])
+        second_index = nums.index(sorted[right]) if sorted[left] != sorted[right] else nums.index(sorted[right], first_index + 1)
+        return [first_index, second_index]
+
+        
+
+# 1. Two Sum
+# Easy
+# Topics
+# Companies
+# Hint
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [2,4,5,8,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+ 
+
+# Constraints:
+
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+ 
+
+# Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sorted = nums.copy()
+        sorted.sort()
+
+        left = 0 
+        right = len(sorted) - 1
+
+        while(sorted[left] + sorted[right] != target):
+            if sorted[left] + sorted[right] > target: 
+                right -= 1 
+            else:
+                left += 1
+        
+        first_index = nums.index(sorted[left])
+        second_index = nums.index(sorted[right]) if sorted[left] != sorted[right] else nums.index(sorted[right], first_index + 1)
+        return [first_index, second_index]
+
+        
+
